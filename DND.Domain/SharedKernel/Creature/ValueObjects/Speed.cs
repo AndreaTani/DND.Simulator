3c90310c24535789062e87d35251145a53a77858
Andrea Tani <adminorama@gmail.com>
2025-10-22 13:45:31 +0200

    Merge AbilityScore and Speed Test (#2)
    * AbilityScore Test class
    
    * Speed Test class
    
    ---------
    
    Co-authored-by: andrea <andrea@andreatani.com>

--- DND.Domain/SharedKernel/Creature/ValueObjects/Speed.cs
+++ DND.Domain/SharedKernel/Creature/ValueObjects/Speed.cs
@@ -2,24 +2,36 @@
 {
     /// <summary>
     /// Speed value object representing movement speed in DND
-    /// It can be divided into three properties: walking, flying and swimming
+    /// It can be divided into three properties: walking, swimming and flying
     /// Almost all creatures have walking speed>0, but flying and swimming are often 0
     /// Each speed type is represented in feet per round with an integer value
     /// </summary>
     public class Speed
     {
-        public int Walking { get; private set; } = 30; // Default walking speed is 30 feet per round
-        public int Flying { get; private set; } = 0;   // Default flying speed is 0 feet per round
-        public int Swimming { get; private set; } = 0; // Default swimming speed is 0 feet per round
+        private int _walking = 30; // Default walking speed is 30 feet per round
+        private int _swimming = 0; // Default swimming speed is 0 feet per round
+        private int _flying = 0;   // Default flying speed is 0 feet per round
+
+        public int Walking { get => _walking; set => _walking = value; }
+        public int Swimming { get => _swimming; set => _swimming = value; }
+        public int Flying { get => _flying; set => _flying = value; }
+
+        // Default Constructor
+        public Speed()
+        {
+            _walking = Walking;
+            _swimming = Swimming;
+            _flying = Flying;
+        }
 
         // Constructor to initialize specific speeds
-        public Speed(int walking, int flying = 0, int swimming = 0)
+        public Speed(int walking, int swimming = 0, int flying = 0)
         {
-            if (walking < 0 || flying < 0 || swimming < 0)
+            if (walking < 0 || swimming < 0 || flying < 0)
                 throw new ArgumentException("Speed values cannot be negative.");
             Walking = walking;
-            Flying = flying;
             Swimming = swimming;
+            Flying = flying;
         }
 
         // Method to update walking speed, returns true if the update was successful
@@ -27,6 +39,14 @@
         {
             if (newWalking < 0) return false;
             Walking = newWalking;
+            return true;
+        }
+
+        // Method to update swimming speed, returns true if the update was successful
+        public bool UpdateSwimming(int newSwimming)
+        {
+            if (newSwimming < 0) return false;
+            Swimming = newSwimming;
             return true;
         }
 
@@ -38,12 +58,5 @@
             return true;
         }
 
-        // Method to update swimming speed, returns true if the update was successful
-        public bool UpdateSwimming(int newSwimming)
-        {
-            if (newSwimming < 0) return false;
-            Swimming = newSwimming;
-            return true;
-        }
     }
 }
